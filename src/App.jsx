112fab92eb48090f200722d911abe716f3b3d1d7
Sourabh U <sourabh.ukkalgaonkar@bacancy.com>
2024-01-29 17:37:21 +0530

    Modify apps component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
-import ProjectsSidebar from "./components/ProjectsSidebar";
+import { useState } from "react";
+import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
+import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
+
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      }
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+  }
+
   return (
-    <main className="h-screen my-8">
-      <ProjectsSidebar/>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
